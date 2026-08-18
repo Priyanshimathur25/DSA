6c758803b03c913cd402ccefa8f8d640e3c3db76
Priyanshimathur25 <mathur.priyanshi005@gmail.com>
2026-08-19 00:32:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (45.01%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,12 +1,16 @@
 class Solution {
     public int arrangeCoins(int n) {
-       int i=1;
-       int res=0;
-       while(n>=i){
-        n-=i;
-        res++;
-        i++;
+       long start=0;
+       long end=n;
+       while(start<=end){
+        long mid=start+(end-start)/2;
+        if(n>=(mid*(mid+1)/2)){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
        }
-       return res;
+       return (int)end;
     }
 }
